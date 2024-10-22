55d8a23ec00f858a85d76b8c738ca45cfefdc820
J <>
2024-10-22 10:51:48 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
-# Samsung IR remote decoding on AVR ATtiny85
+# Samsung IR remote decoding on ATmega328 in Arduino Uno
 
-Program for receiving Samsung IR remote signals on an ATtiny which is then used to control a led strip. The decoding of the signal is done by using interrupts on pin 4 in which an IR sensor is connected. To decode the signal, time between the edges is measured using the internal counter and from that the command is decoded. The value is then used to control a led with PWM. The IR sensor uses timer1 and the PWM led control uses timer0.
+Similar in functionality as the main branch but running on Arduino Uno and uses different pins and timers. Useful for debugging as functions such as `Serial.print` can be used.
 
-The Samsung IR protocol consists of the following:
-- **Start Bit**: 4.5ms ON, 4.5ms OFF
-- **Data Bits**: 32 bits (data 16 bits and address 16 bits)
-  - Logical `1`: 590µs ON, 1690µs OFF
-  - Logical `0`: 590µs ON, 590µs OFF
-- **Stop Bit**: 590µs ON, 590µs OFF
+IR sensor pin = PB0, Arduino pin 8 <br />
+LED pin = PB3, Arduino pin 11
 
-The signal is reversed when capturing it with the IR sensor.
+## Running
 
-## Features
-- Light interrupt handling
-- Minimal global variables
-- Easily adaptable to different AVR microcontrollers
-
-## Setup, Compiling and programming the ATtiny using Arduino Uno as ISP
-
-LED pin = PB0 <br />
-IR sensor pin = PB4
-
-Connect the Arduino to the ATtiny and run the `build.sh` script.
+Copy everything from `main.c` and paste in the Arduino IDE and upload the code.
--- main.c
+++ main.c
@@ -2,66 +2,288 @@
 #include <avr/interrupt.h>
 #include <avr/sfr_defs.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
 
-#include "ir.h"
-#include "pwm.h"
+#define FOSC 16000000            // Clock Speed
+#define TIMER_PRESCALER 1024
+#define TIMER_TICK_uS ((TIMER_PRESCALER * 1000000.0) / FOSC)
 
-// ATtiny at 8MHz
-#define FOSC 8000000
+#define START_BIT_LENGTH (4500.0 / TIMER_TICK_uS)
+#define START_BIT_TOLERANCE (500.0 / TIMER_TICK_uS)
+#define START_BIT_LLIMIT_TICKS ((int)(START_BIT_LENGTH - START_BIT_TOLERANCE))
+#define START_BIT_HLIMIT_TICKS ((int)(START_BIT_LENGTH + START_BIT_TOLERANCE))
+
+#define ON_BIT_THRESHOLD ((int)(1200.0 / TIMER_TICK_uS))
+
+// start bit + 32 data bits + end bit
+#define MSG_LENGTH 2 + 32 * 2 + 1
+
+volatile uint8_t ticks[MSG_LENGTH];
+volatile uint8_t tick_number;
+
+enum
+{
+    ir_one   = 0xFB04,
+    ir_two   = 0xFA05,
+    ir_three = 0xF906,
+    ir_four  = 0xF708,
+    ir_five  = 0xF609,
+    ir_six   = 0xF50A,
+    ir_seven = 0xF30C,
+    ir_eight = 0xF20D,
+    ir_nine  = 0xF10E,
+    ir_zero  = 0xEE11,
+};
+
+enum
+{
+    StartBitLow  = 0,
+    StarBitHigh,
+    StopBit      = 66,
+};
+
+bool is_timer_overflow()
+{
+    return TIFR0 & _BV(TOV0);
+}
+
+void clear_timer_overflow()
+{
+    TIFR0 |= _BV(TOV0);
+}
+
+void reset_timer0()
+{
+    if (is_timer_overflow())
+    {
+        clear_timer_overflow();
+    }
+    TCNT0 = 0;
+}
+
+uint8_t read_timer()
+{
+    return TCNT0;
+}
+
+void reset_msg()
+{
+    memset(ticks, 0, MSG_LENGTH * sizeof(unsigned int));
+    tick_number = 0;
+}
+
+ISR(PCINT0_vect)
+{
+    // if the timer has overflown, the message is not captured correctly or
+    // it is the start of the message. Either way it must be abandoned.
+    if (is_timer_overflow())
+    {
+        reset_timer0();
+        reset_msg();
+        return;
+    }
+
+    uint32_t timer = read_timer();
+    if (tick_number < MSG_LENGTH)
+    {
+        ticks[tick_number] = timer;
+    }
+    tick_number++;
+    reset_timer0();
+}
+
+bool is_start_bit_valid(uint8_t ticks)
+{
+    return (START_BIT_LLIMIT_TICKS < ticks && ticks < START_BIT_HLIMIT_TICKS);
+}
+
+bool message_sane()
+{
+    if (!is_start_bit_valid(ticks[StartBitLow])
+        || !is_start_bit_valid(ticks[StarBitHigh]))
+    {
+        return false;
+    }
+    if (ticks[StopBit] > ON_BIT_THRESHOLD)
+    {
+        return false;
+    }
+    return true;
+}
+
+void decipher_message(uint16_t *data, uint16_t *address)
+{
+    uint32_t message = 0;
+    int data_index = 0;
+    int i = 3;
+    // we are only interested in the UP times which are odd indices
+    // data starts at the fourth indice
+    for (i = 3; i < MSG_LENGTH; i += 2)
+    {
+        if (ticks[i] > ON_BIT_THRESHOLD)
+        {
+            message |= (uint32_t)(1UL << data_index);
+        }
+        data_index++;
+    }
+
+    *address = message;
+    *data = message >> 16;
+}
+
+void init_clock()
+{
+    // set clk/1024
+    TCCR0B |= _BV(CS02) | _BV(CS00);
+}
+
+void init_pins()
+{
+    // set PB0 as input
+    DDRB &= ~_BV(PB0);
+
+    // enable interrupts in pins 7..0
+    PCICR |= _BV(PCIE0);
+    PCMSK0 |= _BV(PCINT0);
+
+}
+
+void dimm_led()
+{
+    if (OCR2A + 20 > UINT8_MAX)
+    {
+        OCR2A = UINT8_MAX;
+        return;
+    }
+    OCR2A = OCR2A + 20;
+}
+
+void brighten_led()
+{
+    if (OCR2A - 20 < 0)
+    {
+        OCR2A = 0;
+        return;
+    }
+    OCR2A = OCR2A - 20;
+}
+
+void led_on()
+{
+    DDRB |= _BV(DDB3);
+}
+
+void led_off()
+{
+    DDRB &= ~_BV(DDB3);
+}
+
+void enable_breathing()
+{
+    // enable timer overflow interrupt
+    TIMSK2 |= _BV(TOIE2);
+}
+
+void disable_breathing()
+{
+    // disable timer overflow interrupt
+    TIMSK2 &= ~_BV(TOIE2);
+}
+
+uint8_t rising = 1;
+ISR(TIMER2_OVF_vect)
+{
+    if (rising)
+    {
+        if (++OCR2A == UINT8_MAX)
+        {
+            rising = 0;
+        }
+    }
+    else
+    {
+        if (--OCR2A == 0)
+        {
+            rising = 1;
+        }
+    }
+}
+
+void init_pwm_timer2()
+{
+    // PINB3 is the OC2A pin
+    DDRB |= _BV(DDB3);
+
+    // start with 50% duty cycle
+    OCR2A |= 128;
+
+    // set fast PWM
+    // Clear OC2A on compare match, set OC2A at BOTTOM, (non-inverting mode).
+    TCCR2A |= _BV(COM2A1) | _BV(WGM20) | _BV(WGM21) | _BV(WGM20);
+
+    // set clk/64
+    TCCR2B |= _BV(CS22) | _BV(CS21) | _BV(CS20);
+
+}
 
 void init()
 {
-    sei();
+    init_clock();
+    init_pins();
+    init_pwm_timer2();
 
-    pwm_init();
-    ir_init();
+    sei();
 }
 
 int main()
 {
     init();
-	while(1)
+	while(true)
 	{
-        if (ir_new_message_received())
+        if (tick_number >= MSG_LENGTH)
         {
-            uint16_t data;
-            uint16_t address;
-            ir_decipher_msg(&data, &address);
-            switch (data)
+            if (message_sane())
             {
-                case ir_one:
+                uint16_t data;
+                uint16_t address;
+                decipher_message(&data, &address);
+                switch (data)
                 {
-                    pwm_disable_breathing();
-                    pwm_dimm_led();
-                    break;
-                }
-                case ir_two:
-                {
-                    pwm_disable_breathing();
-                    pwm_brighten_led();
-                    break;
-                }
-                case ir_three:
-                {
-                    pwm_enable_breathing();
-                    break;
-                }
-                case ir_four:
-                {
-                    pwm_led_off();
-                    break;
-                }
-                case ir_five:
-                {
-                    pwm_led_on();
-                    break;
-                }
-                default:
-                {
-                    break;
+                    case ir_one:
+                    {
+                        disable_breathing();
+                        dimm_led();
+                        break;
+                    }
+                    case ir_two:
+                    {
+                        disable_breathing();
+                        brighten_led();
+                        break;
+                    }
+                    case ir_three:
+                    {
+                        enable_breathing();
+                        break;
+                    }
+                    case ir_four:
+                    {
+                        led_off();
+                        break;
+                    }
+                    case ir_five:
+                    {
+                        led_on();
+                        break;
+                    }
+                    default:
+                    {
+                        break;
+                    }
                 }
             }
-            ir_reset_msg();
+            reset_msg();
         }
 	}
 }
